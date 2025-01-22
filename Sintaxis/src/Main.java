29327f0219db912a26670930d4047d6d3c74f6ca
Akerizz <123045839@upq.edu.mx>
2025-01-22 14:45:23 -0600

    Esructura de un archivo de java

--- Sintaxis/src/Main.java
+++ Sintaxis/src/Main.java
@@ -1,6 +1,14 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Clase Pricipal
 public class Main {
+    //Metodo Main
+    /*
+    soy
+    un comentario
+    de varias
+    lines
+     */
     public static void main(String[] args) {
 
 
@@ -8,11 +16,14 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Jorge Armando Lopez Morales");
+       String Cadenas = "Jorge" + "Lopez" + "Morales";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(Cadenas);
+        System.out.println(Cadenas.length());
+
+        System.out.println(Cadenas.substring(2,5));
+        System.out.println(Cadenas.substring(2));
+        System.out.println(Cadenas.substring(0,5));
+
     }
 }
